a3bc1b5a46895b2755228f26c4ba985e7ba77cf3
NoobSolver <mathuranant123@gmail.com>
2020-09-05 19:15:21 +0530

    Added Components

---
+++ Pokedox/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import Pokegame from './component/Pokegame';
+function App() {
+  return (
+    <div className='App'>
+      <Pokegame />
+    </div>
+  );
+}
+
+export default App;
